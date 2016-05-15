d8b7d1fa3114e76482806aaef2aaa7186d7caec1
mtp <mtp@netcompany.com>
2016-05-15 13:21:33 +0200

    Finished lab 6.3

---
+++ CertificationCode/layouts/Dogs Alive/Comments Form.ascx
@@ -0,0 +1,16 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="Comments_Form.ascx.cs" Inherits="CertificationCode.layouts.Dogs_Alive.Comments_Form" %>
+<%@ Register TagPrefix="sc" Namespace="Sitecore.Web.UI.WebControls" Assembly="Sitecore.Kernel" %>
+
+<div class="form-group">
+    <asp:Label runat="server" CssClass="control-label" ID="lblAuthor" Text="Author" AssociatedControlID="txtAuthor"/>
+    <asp:TextBox runat="server" ID="txtAuthor" CssClass="form-control"/>
+</div>
+
+<div class="form-group">
+    <asp:Label runat="server" ID="lblContent" CssClass="control-label" Text="Comment" AssociatedControlID="txtContent"/>
+    <asp:TextBox runat="server" ID="txtContent" CssClass="form-control"/>
+</div>
+
+<div class="form-group">
+    <asp:Button runat="server" Text="Submit comment" ID="btnSubmit" CssClass="btn btn-default" OnClick="btnSubmit_OnClick"/>
+</div>
---
+++ CertificationCode/layouts/Dogs Alive/Comments Form.ascx.cs
@@ -0,0 +1,46 @@
+﻿using Sitecore;
+using Sitecore.Data;
+using Sitecore.Data.Items;
+using Sitecore.SecurityModel;
+
+namespace CertificationCode.layouts.Dogs_Alive
+{
+    using System;
+
+    public partial class Comments_Form : System.Web.UI.UserControl
+    {
+        private void Page_Load(object sender, EventArgs e)
+        {
+            // Put user code to initialize the page here
+        }
+
+        protected void btnSubmit_OnClick(object sender, EventArgs e)
+        {
+            Database masterDatabase = Sitecore.Configuration.Factory.GetDatabase("master");
+
+            Item contextItem = masterDatabase.GetItem(Sitecore.Context.Item.ID);
+
+            using (new SecurityDisabler())
+            {
+                Item newComment = contextItem.Add(DateUtil.IsoNow,
+                    new TemplateID(new ID("{21B7E48C-3DFF-48A7-B48A-2BE90C9FAB06}")));
+
+                try
+                {
+                    newComment.Editing.BeginEdit();
+
+                    newComment.Fields["Comment Author"].Value = txtAuthor.Text;
+                    newComment.Fields["Comment Text"].Value = txtContent.Text;
+
+                    newComment.Editing.EndEdit();
+                }
+                catch
+                {
+                    newComment.Editing.CancelEdit();
+                }
+            }
+
+            Sitecore.Web.WebUtil.ReloadPage();
+        }
+    }
+}
